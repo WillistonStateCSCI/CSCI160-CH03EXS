d7a65c4a7aa6944b719efe6dfc39cbed4f41cb01
Amanda Skinner <Amanda.Skinner.1@willistonstate.edu>
2025-09-28 15:31:40 -0500

    Add files via upload

---
+++ Constructors.java
@@ -0,0 +1,17 @@
+/* A Demonstration of Using Constructors
+   Anderson, Franceschi
+*/
+
+public class Constructors
+{
+  public static void main( String [] args )
+  {
+    String emptyString;
+    emptyString = new String( );
+ 
+    String greeting;
+    greeting = new String( "Hello" );
+
+    String firstName = new String( "Jane" );
+  }
+}
